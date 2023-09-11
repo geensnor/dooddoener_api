0df4f77831107377b151a60abed43485167b8018
Erik van Zuijdam <35580176+Zuijdam@users.noreply.github.com>
2023-09-11 17:34:16 +0200

    API locatie los

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 // Define the JSON file path
-$jsonFilePath = 'https://raw.githubusercontent.com/geensnor/DigitaleTuin/master/_data/dooddoeners.json';
+include "include.php";
 
 // Read the JSON file
 $jsonData = file_get_contents($jsonFilePath);
@@ -20,12 +20,23 @@ if ($jsonData === false) {
             'error' => 'Failed to parse JSON data.',
         ];
     } else {
-        // Select a random line
-        $randomLine = $lines[array_rand($lines)];
+        // Check if the "expanded" query parameter is set
+        $expanded = isset($_GET['expanded']) && $_GET['expanded'] === 'true';
+
+        // Determine the number of lines to display (default to 1 if "expanded" is not set)
+        $count = $expanded ? 10 : 1;
+
+        // Select random lines based on the count
+        $randomLines = array_rand($lines, $count);
+
+        // If "expanded" is set, $randomLines will be an array; otherwise, it will be a single index
+        $randomLines = is_array($randomLines) ? $randomLines : [$randomLines];
 
         // Prepare the response
         $response = [
-            'random_line' => $randomLine,
+            'random_lines' => array_map(function ($index) use ($lines) {
+                return $lines[$index];
+            }, $randomLines),
         ];
     }
 }
---
+++ include.php
@@ -0,0 +1,4 @@
+<?php
+//Location of the dooddoeners
+$jsonFilePath = 'https://raw.githubusercontent.com/geensnor/DigitaleTuin/master/_data/dooddoeners.json';
+?>
